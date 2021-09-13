fdc57df50f16ab6bb103f350e4f420d0d403aba7
Lrzon <uswork407@gmail.com>
2021-09-13 12:38:16 +0530

    Initial project setup

---
+++ SwiftUIDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDemo
+//
+//  Created by iMac on 11/06/21.
+//  Copyright © 2021 iMac. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
